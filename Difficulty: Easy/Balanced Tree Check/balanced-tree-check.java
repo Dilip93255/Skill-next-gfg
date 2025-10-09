af4b5ecf72c17140356f58d9047b2dede239ffc4
P.DILIP REDDY <pullanagaridilipreddy@gmail.com>
2025-10-09 11:02:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.java
@@ -0,0 +1,28 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean isBalanced(Node root) {
+        // code here
+        return checkHeight(root) != -1;
+    }
+    int checkHeight(Node node) {
+        if (node == null) return 0;
+        int left = checkHeight(node.left);
+        if (left == -1) return -1;
+        int right = checkHeight(node.right);
+        if (right == -1) return -1;
+        if (Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+}
