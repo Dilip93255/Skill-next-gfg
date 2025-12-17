f786d5ddac9475aa76eadf2e50b70a7cb9e6db63
P.DILIP REDDY <pullanagaridilipreddy@gmail.com>
2025-12-17 13:54:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search Pattern/search-pattern.java
@@ -0,0 +1,47 @@
+class Solution {
+    ArrayList<Integer> search(String pat, String txt) {
+        // code here
+        ArrayList<Integer> result= new ArrayList<>();
+        int m=pat.length();
+        int n= txt.length();
+        int[] lps= new int[m];
+        com(pat,m,lps);
+        int i=0,j=0;
+        while(i<n){
+            if(pat.charAt(j) == txt.charAt(i)){
+                i++;
+                j++;
+            }
+            if(j==m){
+                result.add(i-j);
+                j=lps[j-1];
+            }
+            else if(i<n && pat.charAt(j)!=txt.charAt(i)){
+                if(j!=0){
+                    j=lps[j-1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+    static void com(String pat,int m,int[] lps){
+        int len=0,i=1;
+        lps[0]=0;
+        while(i<m){
+            if(pat.charAt(i)==pat.charAt(len)){
+                lps[i++]=++len;
+            }else if(len!=0){
+                len=lps[len-1];
+            }else{
+                lps[i++]=0;
+            }
+        }
+    }
+    public static void main(String args[]){
+        Solution s=new Solution();
+        ArrayList<Integer>  x=s.search("ABABDABACDABABCABAB","ABABCABAB");
+        System.out.println(x);
+    }
+}
